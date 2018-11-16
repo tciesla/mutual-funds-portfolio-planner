34983607885a4f37ddf9e3b65d391e009614893c
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2018-11-16 17:17:48 +0100

    initial commit

---
+++ src/main/kotlin/com/github/tciesla/mutualfundsportfolioplanner/MutualFundsPortfolioPlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.tciesla.mutualfundsportfolioplanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MutualFundsPortfolioPlannerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MutualFundsPortfolioPlannerApplication>(*args)
+}
