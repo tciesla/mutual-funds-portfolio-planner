199ee6bbd10672357aa224b20164cfaedf953498
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2018-11-18 13:05:28 +0100

    portfolio service - refactor

--- src/main/kotlin/com/github/tciesla/mutualfundsportfolioplanner/service/InvestmentPortfolioService.kt
+++ src/main/kotlin/com/github/tciesla/mutualfundsportfolioplanner/service/InvestmentPortfolioService.kt
@@ -48,35 +48,39 @@ class InvestmentPortfolioService {
             investmentStyle: InvestmentStyle
     ): Map<MutualFund.Type, Long> {
 
-        var latestMatchedMutualFundTypeBuckets: Map<MutualFund.Type, Long> = mutualFunds.map { it.type to 0L }.toMap()
 
-        val targetMutualFundTypeBucketsShares: Map<MutualFund.Type, Int> = investmentStyle.mutualFundMixture
+        val investmentStyleMixture: Map<MutualFund.Type, Int> = investmentStyle.mutualFundMixture
 
-        val mutualFundTypeBuckets: MutableMap<MutualFund.Type, Long> = latestMatchedMutualFundTypeBuckets.toMutableMap()
+        var latestBucketsMatchedToInvestmentStyleMixture: Map<MutualFund.Type, Long> = mutualFunds.map { it.type to 0L }.toMap()
+
+        val mutualFundTypeBuckets: MutableMap<MutualFund.Type, Long> = latestBucketsMatchedToInvestmentStyleMixture.toMutableMap()
 
         for (capital in 0L..availableCapital) {
 
             val spentCapital: Double = max(mutualFundTypeBuckets.values.sum().toDouble(), 1.0)
 
-            val bucketsSharesInPortfolio: Map<MutualFund.Type, Double> = mutualFunds
+            val mutualFundTypeBucketsMixture: Map<MutualFund.Type, Double> = mutualFunds
                     .map { it.type to (mutualFundTypeBuckets[it.type]!!.toDouble() / spentCapital) * 100.0 }
                     .toMap()
 
-            if (bucketsSharesInPortfolio.all { it.value.toBigDecimal().compareTo(targetMutualFundTypeBucketsShares[it.key]!!.toBigDecimal()) == 0 }) {
-                latestMatchedMutualFundTypeBuckets = mutualFundTypeBuckets.also { println("matched: $it") }.toMap()
+            if (mutualFundTypeBucketsMixture.all { bucketMixtureMatchesTargetMixture(it, investmentStyleMixture) }) {
+                latestBucketsMatchedToInvestmentStyleMixture = mutualFundTypeBuckets.also { println("matched: $it") }.toMap()
             }
 
-            val bucketsSharesDistancesToTargetShares: Map<MutualFund.Type, Double> = mutualFunds
-                    .map { it.type to targetMutualFundTypeBucketsShares[it.type]!!.toDouble() - bucketsSharesInPortfolio[it.type]!! }
+            val bucketsMixtureDistancesToTargetMixture: Map<MutualFund.Type, Double> = mutualFunds
+                    .map { it.type to investmentStyleMixture[it.type]!!.toDouble() - mutualFundTypeBucketsMixture[it.type]!! }
                     .toMap()
 
-            val farthestMutualFundTypeFromTargetShare: MutualFund.Type = bucketsSharesDistancesToTargetShares.maxBy { it.value }!!.key
-            val capitalInFarthestMutualFundTypeBucket = mutualFundTypeBuckets[farthestMutualFundTypeFromTargetShare]!!
-            mutualFundTypeBuckets[farthestMutualFundTypeFromTargetShare] = capitalInFarthestMutualFundTypeBucket + 1
+            val farthestMutualFundTypeFromTargetMixture: MutualFund.Type = bucketsMixtureDistancesToTargetMixture.maxBy { it.value }!!.key
+            val capitalInFarthestMutualFundTypeBucket = mutualFundTypeBuckets[farthestMutualFundTypeFromTargetMixture]!!
+            mutualFundTypeBuckets[farthestMutualFundTypeFromTargetMixture] = capitalInFarthestMutualFundTypeBucket + 1
         }
 
-        return latestMatchedMutualFundTypeBuckets
+        return latestBucketsMatchedToInvestmentStyleMixture
     }
+
+    private fun bucketMixtureMatchesTargetMixture(it: Map.Entry<MutualFund.Type, Double>, targetMutualFundTypeBucketsShares: Map<MutualFund.Type, Int>) =
+            it.value.toBigDecimal().compareTo(targetMutualFundTypeBucketsShares[it.key]!!.toBigDecimal()) == 0
 
     private fun createPortfolioItems(
             mutualFunds: List<MutualFund>,
@@ -84,24 +88,39 @@ class InvestmentPortfolioService {
             mutualFundTypeBuckets: Map<MutualFund.Type, Long>
     ): List<InvestmentPortfolio.Item> {
 
-        val splitCapitalPerMutualFund: MutableMap<MutualFund, Long> = mutualFunds.map { mutualFund ->
-            mutualFund to mutualFundTypeBuckets[mutualFund.type]!! / mutualFunds.count { it.type == mutualFund.type }
-        }.toMap().toMutableMap()
+        val mutualFundInvestments: MutableMap<MutualFund, Long> = splitCapitalPerMutualFund(
+                mutualFunds, mutualFundTypeBuckets).toMutableMap()
 
+        appendRemainingCapitalToFirstMutualFundWithType(mutualFundTypeBuckets, mutualFundInvestments)
 
-        mutualFundTypeBuckets.map { (mutualFundType, capitalInBucket) ->
-            mutualFundType to capitalInBucket - splitCapitalPerMutualFund.filterKeys { it.type == mutualFundType }.values.sum()
-        }.forEach { (mutualFundType, remainingCapital) ->
-            val firstMutualFundWithType = splitCapitalPerMutualFund.filterKeys { it.type == mutualFundType }.entries.first()
-            splitCapitalPerMutualFund[firstMutualFundWithType.key] = firstMutualFundWithType.value + remainingCapital
-        }
-
-        return splitCapitalPerMutualFund.map { (mutualFund, capitalInvestedInMutualFund) -> InvestmentPortfolio.Item(
+        return mutualFundInvestments.map { (mutualFund, capitalInvestedInMutualFund) -> InvestmentPortfolio.Item(
                 mutualFund = mutualFund,
                 investedCapital = capitalInvestedInMutualFund,
                 portfolioShare = ((capitalInvestedInMutualFund.toDouble() / investedCapital.toDouble()) * 100.0).round())
         }
 
+    }
+
+    private fun splitCapitalPerMutualFund(
+            mutualFunds: List<MutualFund>,
+            investedCapitalPerMutualFundType: Map<MutualFund.Type, Long>
+    ): Map<MutualFund, Long> = mutualFunds.map { mutualFund ->
+        val mutualFundsCountWithType: Int = mutualFunds.count { it.type == mutualFund.type }
+        val investedCapitalInMutualFundType = investedCapitalPerMutualFundType[mutualFund.type]!!
+        mutualFund to investedCapitalInMutualFundType / mutualFundsCountWithType
+    }.toMap()
+
+    private fun appendRemainingCapitalToFirstMutualFundWithType(
+            investedCapitalPerMutualFundType: Map<MutualFund.Type, Long>,
+            mutualFundInvestments: MutableMap<MutualFund, Long>
+    ) {
+
+        investedCapitalPerMutualFundType.map { (mutualFundType, investedCapitalInType) ->
+            mutualFundType to investedCapitalInType - mutualFundInvestments.filterKeys { it.type == mutualFundType }.values.sum()
+        }.forEach { (mutualFundType, remainingCapital) ->
+            val firstMutualFundWithType = mutualFundInvestments.filterKeys { it.type == mutualFundType }.entries.first()
+            mutualFundInvestments[firstMutualFundWithType.key] = firstMutualFundWithType.value + remainingCapital
+        }
     }
 
     private fun Double.round(): Double = this.toBigDecimal().setScale(5, RoundingMode.HALF_EVEN).toDouble()
